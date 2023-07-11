1dd6985aee33c19b8d0b992fb46534bd2b6b2bd6
Laz <nyys811@aol.com>
2023-07-10 22:32:57 -0400

    added function for setting up main menu of app

--- cli.js
+++ cli.js
@@ -8,3 +8,52 @@ const {
   addEmployee,
   updateEmployeeRole
 } = require('./queries');
+
+function mainMenu() {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'action',
+          message: 'What would you like to do?',
+          choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit'
+          ]
+        }
+      ])
+      .then((answer) => {
+        switch (answer.action) {
+          case 'View all departments':
+            viewAllDepartments();
+            break;
+          case 'View all roles':
+            viewAllRoles();
+            break;
+          case 'View all employees':
+            viewAllEmployees();
+            break;
+          case 'Add a department':
+            promptAddDepartment();
+            break;
+          case 'Add a role':
+            promptAddRole();
+            break;
+          case 'Add an employee':
+            promptAddEmployee();
+            break;
+          case 'Update an employee role':
+            promptUpdateEmployeeRole();
+            break;
+          case 'Exit':
+            console.log('Goodbye!');
+            process.exit(0);
+        }
+      });
+  }
