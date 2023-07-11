25adedabc18077dddab3df81388843a9ca7d3d6a
Laz <nyys811@aol.com>
2023-07-10 22:18:28 -0400

    created tables with primary and foreign keys, also created database in mysql shell

--- schema.sql
+++ schema.sql
@@ -3,3 +3,29 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
 
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name VARCHAR(30)
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
