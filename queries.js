fcc97bd2390f15472c33c21c3cdd7a7e3013d59a
Laz <nyys811@aol.com>
2023-07-10 22:25:03 -0400

    added all queries for retrieving all departments, roles, and employees

--- queries.js
+++ queries.js
@@ -1,1 +1,13 @@
 const connection = require('./connection');
+
+function getAllDepartments() {
+    return connection.query('SELECT * FROM department');
+  }
+  
+  function getAllRoles() {
+    return connection.query('SELECT * FROM role');
+  }
+  
+  function getAllEmployees() {
+    return connection.query('SELECT * FROM employee');
+  }
