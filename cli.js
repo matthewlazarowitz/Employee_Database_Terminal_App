19b3e72cfac615e287cc57d1063dd8e1e179e79b
Laz <nyys811@aol.com>
2023-07-10 22:36:27 -0400

    added function to add employee

--- cli.js
+++ cli.js
@@ -140,3 +140,55 @@ function mainMenu() {
           .catch((err) => console.error(err));
       });
   }
+
+  function promptAddEmployee() {
+    inquirer
+      .prompt([
+        {
+          name: 'first_name',
+          message: 'Enter the first name of the employee:',
+        },
+        {
+          name: 'last_name',
+          message: 'Enter the last name of the employee:',
+        },
+        {
+          name: 'role_id',
+          message: 'Enter the role ID for the employee:',
+          validate: (value) => {
+            if (isNaN(value)) {
+              return 'Please enter a valid role ID';
+            }
+            return true;
+          }
+        },
+        {
+          name: 'manager_id',
+          message: 'Enter the manager ID for the employee (leave empty if none):',
+          validate: (value) => {
+            if (value !== '' && isNaN(value)) {
+              return 'Please enter a valid manager ID or leave it empty';
+            }
+            return true;
+          }
+        }
+      ])
+      .then((answer) => {
+        
+        const managerId = answer.manager_id === '' ? null : answer.manager_id;
+  
+        const employee = {
+          first_name: answer.first_name,
+          last_name: answer.last_name,
+          role_id: answer.role_id,
+          manager_id: managerId
+        };
+  
+        addEmployee(employee)
+          .then(() => {
+            console.log('Employee added successfully!');
+            mainMenu();
+          })
+          .catch((err) => console.error(err));
+      });
+  }
